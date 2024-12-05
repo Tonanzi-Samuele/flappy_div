e9bf8328a1b8d4bdbabd67f46944c54e6d904ad1
Andruetto-Lorenzo <lorenzo.andruetto@istitutoagnelli.it>
2024-12-05 20:07:06 +0100

    added changes

--- flappy_DIV/play.js
+++ flappy_DIV/play.js
@@ -1,81 +1,129 @@
+// funzione per rimuovere la classe ad un oggetto
+function removeClass(obj, classe) {
+    return obj.classList.remove(classe);
+}
+
+// funzione per aggiungere la classe ad un oggetto
 function addClass(obj, classe) {
     return obj.classList.add(classe);
 }
 
+// funzione per creare un elemento HTML
 function create(element) {
     return document.createElement(element);
 }
 
+// funzione per ottenere oggetto da querySelector
 function querySelector(obj) {
     return document.querySelector(obj);
 }
 
-document.addEventListener('DOMContentLoaded',() =>{
-const bird = querySelector(".flappy");
-const displayGioco = querySelector(".container");
-const ground = querySelector(".pavimento");
+document.addEventListener('DOMContentLoaded', () => {
+    const bird = querySelector(".flappy");
+    const displayGioco = querySelector(".container");
+    const ground = querySelector(".ground-moving");
 
-let birdSx = 220;
-let birdBot = 100;
-let gravita = 2;
-let game_over = false;
-let gap = 430
+    let birdSx = 220;
+    let birdBot = 100;
+    let gravita = 2;
+    let game_over = false;
+    let gap = 430;
 
     function startGioco() {
         birdBot -= gravita;
         bird.style.bottom = birdBot + "px";
         bird.style.left = birdSx + "px";
     }
-    let idTempoGioco = setInterval(startGioco, 20);
+    const idTempoGioco = setInterval(startGioco, 20);
 
-function generaostacolo(){
-    let ostacoloSx = 500;
-    let Altezza = Math.random() * 60;
-    let ostacoloBot = Altezza;
-    const ostacolo = create("div");
-    const ostacolosopra = create("div");
-    if (!game_over){
-        addClass(ostacolo, 'ostacolo');
-        addClass(ostacolosopra, 'ostacolosopra');
+    function control(e){
+        if(e.keyCode === 32) {
+            salta();
+        }
     }
-    displayGioco.appendChild(ostacolo);
-    displayGioco.appendChild(ostacolosopra);
-    ostacolo.style.left = ostacoloSx + "px";
-    ostacolosopra.style.left = ostacoloSx + "px";
-    ostacolo.style.bottom = ostacoloBot + "px";
-    ostacolosopra.style.bottom = ostacoloBot + gap + "px";
 
-    
-    function muoviostacolo(){
-        ostacoloSx -= 2;
+    function salta(){
+        if(birdBot < 500) birdBot += 50;
+        bird.style.bottom = birdBot + 'px';
+        console.log(birdBot);
+    }
+    document.addEventListener('keyup', control);
+
+    function generaostacolo() {
+        let ostacoloSx = 500;
+        let height = Math.random() * 60;
+        let ostacoloBot = height;
+        const ostacolo = create("div");
+        const ostacolosopra = create("div");
+
+        if (!game_over) {
+            ostacolo.classList.add('ostacolo');
+            ostacolosopra.classList.add('ostacolosopra');
+            addClass(ostacolo, "ostacolo");
+            addClass(ostacolosopra, "ostacolosopra");
+        }
+
+        displayGioco.appendChild(ostacolo);
+        displayGioco.appendChild(ostacolosopra);
         ostacolo.style.left = ostacoloSx + "px";
         ostacolosopra.style.left = ostacoloSx + "px";
+        ostacolo.style.bottom = ostacoloBot + "px";
+        ostacolosopra.style.bottom = ostacoloBot + gap + "px";
 
-        console.log(ostacoloSx)
-        if(ostacoloSx ===  0){
-            clearInterval(timerId);
-            displayGioco.removeChild(ostacolo);
-            displayGioco.removeChild(ostacolosopra);
-        }
-        if(ostacoloSx > 200 && ostacoloSx < 280 && birdSx === 220 && (birdBot < ostacoloBot + 153 || birdBot > ostacoloBot + gap -200 ) || birdBot === 0){
-            gameover();
-            clearInterval(inter);
-        }
-        inter = setInterval(muoviostacolo,20);
-        setTimeout(generaostacolo, 3000);
-    }
-    let timerId = setInterval(muoviostacolo,20);
-    //!game_over ? setTimeout(generaostacolo, 3000)
-    if (!game_over){
-        setTimeout(generaostacolo, 2700);
-    }
-}
-generaostacolo();
+        function muoviostacolo() {
+            ostacoloSx -= 2;
+            ostacolo.style.left = ostacoloSx + "px";
+            ostacolosopra.style.left = ostacoloSx + "px";
 
-function gameover(){
-    clearInterval(idTempoGioco)
-    game_over == true;
-    document.removeEventListener("keyup", control)
-}
-}
-)
+            // console.log(ostacoloSx)
+            if (ostacoloSx === -60) {
+                clearInterval(timerId);
+                displayGioco.removeChild(ostacolo);
+                displayGioco.removeChild(ostacolosopra);
+            } if (
+                ostacoloSx > 200 && ostacoloSx < 280 && birdSx === 220 && 
+                (
+                    birdBot < ostacoloBot + 153 || 
+                    birdBot > ostacoloBot + gap - 200
+                ) || birdBot === 0
+            ) {
+                gameover();
+                clearInterval(timerId);
+            }
+        }
+        let timerId = setInterval(muoviostacolo, 20);
+        //!game_over ? setTimeout(generaostacolo, 3000)
+        if (!game_over)
+            setTimeout(generaostacolo, 2700);
+    }
+    generaostacolo();
+
+    function gameover() {
+        // blocco di codice che stoppa flappy dal poter volare
+        clearInterval(idTempoGioco);
+        game_over == true;
+        document.removeEventListener("keyup", control);
+
+        addClass(ground, 'pavimento');
+        removeClass(ground, 'ground-moving');
+
+        // blocco di codice per la schermata di Game Over.
+        let gameOverText = create('h1');
+        let divGameOver = querySelector('.gameover-text');
+        let buttons = create('div');
+        let homeButton = create('a');
+        let replay = create('a');
+
+        addClass(buttons, 'gameover-buttons')
+        gameOverText.innerText = 'GAME OVER!';
+        homeButton.innerText = 'Home';
+        homeButton.href = './homepage.html';
+
+        replay.innerText = 'Retry';
+        replay.href = './game.html';
+
+        gameOverText.id = 'gameOver-Text';
+        buttons.append(homeButton, replay);
+        divGameOver.after(gameOverText, buttons);
+    }
+})
