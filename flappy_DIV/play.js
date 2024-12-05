f80f521de123e54194c5fad123a9dae1ca3dedcb
Tonanzi-Samuele <samuele.github@gmail.com>
2024-12-05 16:02:19 +0100

    commit

--- flappy_DIV/play.js
+++ flappy_DIV/play.js
@@ -3,7 +3,6 @@ function querySelector(obj) {
 }
 
 document.addEventListener('DOMContentLoaded',() =>{
-<<<<<<< HEAD
 const bird = document.querySelector(".flappy");
 const displayGioco = document.querySelector(".container");
 const ground = document.querySelector(".pavimento");
@@ -13,17 +12,6 @@ let birdBot = 100;
 let gravita = 2;
 let game_over = false;
 let gap = 430
-=======
-    let inter = 0;
-    const bird = querySelector(".flappy");
-    const displayGioco = querySelector(".container");
-    const ground = querySelector(".pavimento");
-
-    let birdSx = 220;
-    let birdBot = 100;
-    let gravita = 2;
-    let game_over = false;
->>>>>>> refs/remotes/origin/main
 
     function startGioco() {
         birdBot -= gravita;
@@ -32,7 +20,6 @@ let gap = 430
     }
     let idTempoGioco = setInterval(startGioco, 20);
 
-<<<<<<< HEAD
 function generaostacolo(){
     let ostacoloSx = 500;
     let Altezza = Math.random() * 60;
@@ -65,47 +52,10 @@ function generaostacolo(){
         if(ostacoloSx > 200 && ostacoloSx < 280 && birdSx === 220 && (birdBot < ostacoloBot + 153 || birdBot > ostacoloBot + gap -200 ) || birdBot === 0){
             gameover();
             clearInterval(inter);
-=======
-    function control(e){
-        if (e.keyCode === 32){
-            salto()
-        }
-    }
-
-    function salto(){
-        if(birdBot < 500) birdBot += 50
-        bird.style.bottom = birdBot + "px";
-    }
-
-    document.addEventListener("keyup", control);
-
-    function generaostacolo(){
-        let ostacoloSx = 500;
-        let Altezza = Math.random() * 60;
-        let ostacoloBot = Altezza;
-        const ostacolo = document.createElement("div");
-        ostacolo.classList.add("ostacolo");
-        displayGioco.appendChild(ostacolo);
-        ostacolo.style.left = ostacoloSx + "px";
-        ostacolo.style.bottom = ostacoloBot + "px";
-
-        function muoviostacolo(){
-            ostacoloSx -= 2;
-            ostacolo.style.left = ostacoloSx + "px";
-            console.log(ostacoloSx)
-            if(ostacoloSx ===  0){
-                clearInterval(inter);
-                displayGioco.removeChild(ostacolo);
-            }
-            if(ostacoloSx > 200 && ostacoloSx < 280 && birdSx === 200 || birdBot === 0){
-                gameover(); 
-            }
->>>>>>> refs/remotes/origin/main
         }
         inter = setInterval(muoviostacolo,20);
         setTimeout(generaostacolo, 3000);
     }
-<<<<<<< HEAD
     let timerId = setInterval(muoviostacolo,20);
     //!game_over ? setTimeout(generaostacolo, 3000)
     if (!game_over){
@@ -113,14 +63,10 @@ function generaostacolo(){
     }
 }
 generaostacolo();
-=======
-    generaostacolo();
->>>>>>> refs/remotes/origin/main
 
     function gameover(){
         clearInterval(idTempoGioco)
 
-<<<<<<< HEAD
 
 function gameover(){
     clearInterval(idTempoGioco)
@@ -128,9 +74,5 @@ function gameover(){
     document.removeEventListener("keyup", control)
 }
 
-=======
-        game_over == true;
-        document.removeEventListener("keyup", control)
-    }
->>>>>>> refs/remotes/origin/main
+}
 })
